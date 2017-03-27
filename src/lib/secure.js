e14b021283355a889f3002bf7f245cdcb6a11e42
Alex <ivanov.alexey.92@gmail.com>
2017-03-27 22:55:31 +0300

    Init project

---
+++ src/lib/secure.js
@@ -0,0 +1,8 @@
+import crypto from 'crypto';
+
+const secret = 'abcdefg';
+const encrypt = value => crypto.createHmac('sha256', secret)
+  .update(value)
+  .digest('hex');
+
+export default encrypt;
